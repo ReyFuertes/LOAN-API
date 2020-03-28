22fee08a8bbd709c3e297cec6a0a70860d3874e9
RFuertes <reynelfuertes@gmail.com>
2020-03-28 16:14:47 +0800

    refactor sql criteria to accept or/and operations

--- src/borrowers/borrowers.repository.ts
+++ src/borrowers/borrowers.repository.ts
@@ -10,15 +10,26 @@ export class BorrowersRepository extends Repository<Borrower> {
   async getFilterBorrowers(getBorrowerFilterDto: GetBorrowerFilterDto): Promise<Borrower[]> {
     const query = this.createQueryBuilder('borrower');
     _.mapValues(getBorrowerFilterDto, _.method('toLowerCase')); // convert values to lowercases
+    const where = getBorrowerFilterDto;
     const page = Object.assign({}, {
       take: getBorrowerFilterDto.take,
       skip: getBorrowerFilterDto.skip
     });
-    if (getBorrowerFilterDto && Object.keys(getBorrowerFilterDto)) {
-      query.where(getBorrowerFilterDto); // criteria
+    delete where.skip, where.take;
+
+    if (where && Object.keys(where)) {
+      Object.entries(where).forEach(c => {
+        //transform entries into object
+        const obj = Object.assign({}, Object.entries(c)
+          .reduce((acc, [k, v]) => ({ ...acc, [c[0]]: v }), {})
+        );
+        //note: im just using orWhere so every criteria will match the database
+        query.orWhere(`${Object.keys(obj)} ilike :${Object.keys(obj)}`, obj)
+      });
     }
     const borrowers = await query
       .skip(page.skip).take(page.take) // pagination
+      .printSql() //debug sql
       .getRawMany();
     return borrowers;
   }
